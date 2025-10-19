c9e24985dae325888d4a1f207f6012f5df5fd3ed
lunakenya <luna28kenya@gmail.com>
2025-10-18 21:09:48 -0500

    Update de file next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // CRÍTICO: Indica a Next.js que genere el output en modo autónomo (standalone).
+  // Esto es necesario para la optimización y despliegue dentro de un contenedor Docker.
+  output: 'standalone', 
 };
 
 export default nextConfig;
